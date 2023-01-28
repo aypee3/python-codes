09360b7fc1a0c839669fcd29c8c7677610dec62b
aypee3 <aypee3@gmail.com>
2023-01-28 16:55:49 +0530

    Added files

---
+++ poem.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{   
+    printf("Baa, baa black sheep\n");
+    printf("Have you any wool\n");
+    printf("Yes sir, yes sir\n");
+    printf("Three bags full\n");
+    return 0;
+}
